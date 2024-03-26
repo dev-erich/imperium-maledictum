e2eb85b5b20772e13ad7bf4e6af7cf421f2f5c8d
Eric Heinze <eheinze920@gmail.com>
2024-03-26 09:43:36 -0600

    fixed import

--- src/routes/CharacterEditor/Tabs/index.ts
+++ src/routes/CharacterEditor/Tabs/index.ts
@@ -1,5 +1,4 @@
 export { default as CharacterTabs } from './CharacterTabs'
-export { type CharacterTabProps } from './CharacterTabs'
 
 export { Details } from './Details'
 export { Characteristics } from './Characteristics'
